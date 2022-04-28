19e5b1a1f1e0a39d05f883becd2c031e63f2551f
DEEP PATEL <cdpatel1905@gmail.com>
2022-04-27 21:40:00 -0400

    first commit

---
+++ src/test/java/tests/Testing1.java
@@ -0,0 +1,10 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class Testing1 {
+  @Test
+  public void f() {
+	  System.out.println("charmi");
+  }
+}
---
+++ src/test/java/tests/Testing2.java
@@ -0,0 +1,10 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class Testing2 {
+  @Test
+  public void f() {
+	  System.out.println("Deep");
+  }
+}
